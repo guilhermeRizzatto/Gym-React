37f897ac4d10decf3b79cb1f8529d2391f557dfd
Guilherme <guimarquesrz@gmail.com>
2024-02-02 21:13:19 -0300

    Configure Links in ListBar

--- src/pages/Universal/ListBar.js
+++ src/pages/Universal/ListBar.js
@@ -7,13 +7,13 @@ function ListBar(){
         <div className="listBar">
             <span id="SpanMenu">Menu</span>
             <ul className="Buttons">
-                <Link to='http://localhost:3000/member'><button id="gym-button" type="button">Gym Memberships</button></Link>
-                <Link to='http://localhost:3000/registrations'><button id="regi-button">Registrations</button></Link>
-                <Link to='http://localhost:3000/trainers'><button id="train-button">Trainers</button></Link>
-                <Link to='http://localhost:3000/workLoads'><button id="workL-button">WorkLoads</button></Link>
-                <Link to='http://localhost:3000/workouts'><button id="workts-button">Workouts</button></Link>
-                <Link to='http://localhost:3000/exercises'><button id="exerc-button">Exercises</button></Link>
-                <Link to='http://localhost:3000/exerciseTypes'><button id="exerType-button">ExercisesTypes</button></Link>
+                <Link to='/member'><button id="gym-button" type="button">Gym Memberships</button></Link>
+                <Link to='/registrations'><button id="regi-button">Registrations</button></Link>
+                <Link to='trainers'><button id="train-button">Trainers</button></Link>
+                <Link to='/workLoads'><button id="workL-button">WorkLoads</button></Link>
+                <Link to='/workouts'><button id="workts-button">Workouts</button></Link>
+                <Link to='/exercises'><button id="exerc-button">Exercises</button></Link>
+                <Link to='/exerciseTypes'><button id="exerType-button">ExercisesTypes</button></Link>
             </ul>
         </div>
     )
